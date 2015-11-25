acc52cbd56bb8f87b096895616ea599870daf1dc
Tim Channell <tim.channell@gmail.com>
2015-11-25 10:37:14 -0500

    fixed typo in download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p data2
+mkdir -p data
 curl -o data/latest.planet.mbtiles.gz https://s3.amazonaws.com/mapbox/osm-qa-tiles/latest.planet.mbtiles.gz
 cd data 
 gunzip latest.planet.mbtiles
